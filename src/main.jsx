f683f045ca43f78688f5ee7171e1113161d948a9
AmyLien065 <amy.lien0605@gmail..com>
2025-08-18 00:02:42 +0800

    0817-Amy-main改回

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 // import './index.css';
-// import App from './App.jsx';
-import BarInnerPage from './pages/BarInnerPage.jsx';
+import App from './App.jsx';
+// import BarInnerPage from './pages/BarInnerPage.jsx';
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <HashRouter>
-      <BarInnerPage />
+      <App />
     </HashRouter>
   </StrictMode>,
 )
