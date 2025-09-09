3e73bc2453ff6dc6f034dd19b4911b6b25e6fe1f
AmyLien065 <amy.lien0605@gmail..com>
2025-09-09 17:20:04 +0800

    0909-酒吧內頁RWD-v1

--- src/pages/BarInnerPage.jsx
+++ src/pages/BarInnerPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 // import '../css/style.css';
 import '../css/BarInnerPage.css';
 import Navbar from '../component/Navbar';
@@ -15,84 +16,85 @@ const BarInnerPage = () => {
         <>
 
             <main className="barInnerPage">
-                {/* Hero 區 */}
-                <section id="hero" className="heroContainer">
+                <div className='barInnerPageContainer'>
+                    {/* Hero 區 */}
+                    <section id="barInnerHero" className="barInnerHeroContainer">
 
-                    {/* 左側：hero酒吧文案 */}
-                    <div className="cardLeft">
-                        <h2>潮飲空間</h2>
+                        {/* 左側：hero酒吧文案 */}
+                        <div className="barInnerCardLeft">
+                            <h2>潮飲空間</h2>
 
-                        {/* 連絡資訊：語意化 */}
-                        <address className="contact" /* SCSS 再把預設斜體改回正常 */>
-                            <p>地址：新北市新北一路二段15樓</p>
-                            <p>電話：0949-494-949
-                                {/* <a href="tel:0949494949"></a> 這是電擊電話號碼會自動跳轉播號的超連結功能 */}
-                            </p>
-                            <p>營業時間：20:00–02:00</p>
-                        </address>
+                            {/* 連絡資訊*/}
+                            <address className="barInnerContact" /* SCSS 再把預設斜體改回正常 */>
+                                <p>地址：新北市新北一路二段15樓</p>
+                                <p>電話：0949-494-949</p>
+                                <p>營業時間：20:00–02:00</p>
+                            </address>
 
-                        <div className="description">
-                            <p>在暖黃燈下，甬管柔唱輕撫心弦，貝斯低吟</p>
-                            <p>穿越夜色。今宵經典爵士之夜，現場薩克斯</p>
-                            <p>與鋼琴共舞，細品調和雞尾酒，讓律動與酒</p>
-                            <p>香交織。邀您相約週五晚，於吧檯前舉杯，</p>
-                            <p>沉浸不朽樂章，為平凡夜晚增添永恆樂章。</p>
+                            {/* 左側hero酒吧文案*/}
+                            <div className="barInnerDescription">
+                                <p>在暖黃燈下，甬管柔唱輕撫心弦，貝斯低吟</p>
+                                <p>穿越夜色。今宵經典爵士之夜，現場薩克斯</p>
+                                <p>與鋼琴共舞，細品調和雞尾酒，讓律動與酒</p>
+                                <p>香交織。邀您相約週五晚，於吧檯前舉杯，</p>
+                                <p>沉浸不朽樂章，為平凡夜晚增添永恆樂章。</p>
+                            </div>
                         </div>
+
+                        {/* 右側：hero酒吧圖片 */}
+                        <div className="barInnerCardRight">
+                            <figure className="barInnerHeroPhoto">
+                                <img className="barInnerHeroImg" src={heroImg} alt="潮飲空間圖片" />
+                                <img className="barInnerArrowImg1" src={arrowImg1} alt="裝飾箭頭1" />
+                            </figure>
+                        </div>
+
+                    </section>
+
+                    {/* 推薦酒吧區 */}
+                    <section id="recommendations" className="barInnerRecommendationsContainer">
+                        <figure className='barInnerLeftArrow'>
+                            <img src={arrowImg2} alt="裝飾箭頭2" style={{ width: '100px' }} />
+                        </figure>
+                        <div className='barInnerRecPhotos'>
+                            <button className="barInnerBtnLeft">
+                                <img src={arrowImg3} alt="向左滑動" />
+                            </button>
+
+                            {/* 左側：推薦酒吧1+文案 */}
+                            <figure className="barInnerRecPhoto1">
+                                <img src={rec1Img} alt="The Gilded Drop圖片" />
+                                <figcaption>
+                                    <p className="barInnerCaptionText">
+                                        The Gilded Drop — 在暖黃燈下，甬管柔唱輕撫心弦，貝斯在暖黃燈下，甬管弦，貝斯在暖在暖黃燈下低...
+                                    </p>
+                                </figcaption>
+                            </figure>
+
+                            {/* 右側：推薦酒吧2+文案 */}
+                            <figure className="barInnerRecPhoto2">
+                                <img src={rec2Img} alt="時光行者圖片" />
+                                <figcaption>
+                                    <p className="barInnerCaptionText">
+                                        時光行者 — 在暖黃燈下，甬管柔唱輕撫心弦，貝斯在暖黃燈下，甬管弦，貝斯在暖在暖黃燈下低...
+                                    </p>
+                                </figcaption>
+                            </figure>
+                        </div>
+                    </section>
+
+                    {/* 按鈕容器 */}
+                    <div className='barInnerBtnContainer'>
+                        <Link to="/Bar" className="barInnerBtnToListPage">
+                            回到酒吧列表
+                        </Link>
                     </div>
 
-                    {/* 右側：hero酒吧圖片 */}
-                    <div className="cardRight">
-                        <figure className="heroPhoto">
-                            <img className="heroImg" src={heroImg} alt="潮飲空間圖片" style={{ width: '850px' }} />
-                            <img className="arrowImg1" src={arrowImg1} alt="裝飾箭頭1" />
-                        </figure>
+                    {/* 純粹只是背景板的一個黑色區塊 */}
+                    <div className="barInnerBlackBlock">
                     </div>
 
-                </section>
-
-                {/* 推薦酒吧區 */}
-                <section id="recommendations" className="recommendationsContainer">
-                    <figure className='leftArrow'>
-                        <img src={arrowImg2} alt="裝飾箭頭2" style={{ width: '100px' }} />
-                    </figure>
-                    <div className='recPhotos'>
-                        <button className="btnLeft">
-                            <img src={arrowImg3} alt="向左滑動" />
-                        </button>
-
-
-                        {/* 左側：推薦酒吧1+文案 */}
-                        <figure className="recPhoto1">
-                            <img src={rec1Img} alt="The Gilded Drop圖片" />
-                            <figcaption>
-                                <p className="captionText">
-                                    The Gilded Drop — 在暖黃燈下，甬管柔唱輕撫心弦，貝斯在暖黃燈下，甬管弦，貝斯在暖在暖黃燈下低...
-                                </p>
-                            </figcaption>
-                        </figure>
-
-                        {/* 右側：推薦酒吧2+文案 */}
-                        <figure className="recPhoto2">
-                            <img src={rec2Img} alt="時光行者圖片" />
-                            <figcaption>
-                                <p className="captionText">
-                                    時光行者 — 在暖黃燈下，甬管柔唱輕撫心弦，貝斯在暖黃燈下，甬管弦，貝斯在暖在暖黃燈下低...
-                                </p>
-                            </figcaption>
-                        </figure>
-                    </div>
-                </section>
-
-                {/* 按鈕容器 */}
-                <div className='btnContainer'>
-                    <button className="btnToListPage">回到熱門圖鑑</button>
                 </div>
-
-                {/* 純粹只是背景板的一個黑色區塊 */}
-                <div className="blackBlock">
-                </div>
-
-
             </main>
 
         </>
